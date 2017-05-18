35bd32a3b2c37bc202cef75e9c57b6a48ee450dd
fanlv34 <bladelocker@126.com>
2017-05-18 23:03:22 +0800

    master modify MyTest

--- test/src/test/MyTest.java
+++ test/src/test/MyTest.java
@@ -6,6 +6,7 @@ public class MyTest {
 		// TODO Auto-generated method stub
 		System.out.println("gogo");
 		System.out.println("gogo2");
+		System.out.println("add sub master branch");
 		System.out.println("add dev branch");
 	}
 
